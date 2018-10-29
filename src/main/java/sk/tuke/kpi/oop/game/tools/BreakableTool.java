62e51308a30392f525b416dd2e66770cc42f9953
br126 <jahoa123>
2018-10-29 14:52:05 +0100

    +added new package sk.tuke.kpi.oop.game.actions also created new class for representig of actions

--- src/main/java/sk/tuke/kpi/oop/game/tools/BreakableTool.java
+++ src/main/java/sk/tuke/kpi/oop/game/tools/BreakableTool.java
@@ -1,4 +1,4 @@
-package sk.tuke.kpi.oop.game;
+package sk.tuke.kpi.oop.game.tools;
 
 import sk.tuke.kpi.gamelib.framework.AbstractActor;
 
@@ -11,6 +11,11 @@ public abstract class BreakableTool extends AbstractActor {
     }
 
     public void use(){
-
+        if(remainingUses > 0){
+            remainingUses--;
+            if(remainingUses == 0) {
+                this.getScene().removeActor(this);
+            }
+        }
     }
 }
