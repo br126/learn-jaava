440e7117550c188379c977985d56d032d36e97ae
Branislav <branorohacek@gmail.com>
2018-11-02 20:55:17 +0100

    Krok 4: Defective Light -  finished!

--- src/main/java/sk/tuke/kpi/oop/game/DefectiveLight.java
+++ src/main/java/sk/tuke/kpi/oop/game/DefectiveLight.java
@@ -1,4 +1,37 @@
 package sk.tuke.kpi.oop.game;
 
+import org.jetbrains.annotations.NotNull;
+import sk.tuke.kpi.gamelib.Scene;
+import sk.tuke.kpi.gamelib.actions.Invoke;
+import sk.tuke.kpi.gamelib.framework.actions.Loop;
+
+import java.util.Random;
+
 public class DefectiveLight extends Light {
+
+    private Random rand;
+    private int randInt;
+
+    public DefectiveLight(){
+        rand = new Random();
+        randInt = 0;
+    }
+
+    @Override
+    public void addedToScene(@NotNull Scene scene) {
+        super.addedToScene(scene);
+        //new Invoke(this::coolReactor).scheduleOn(this);
+        //coolReactor();
+        new Loop<>(new Invoke(this::simulateDefectiveLight)).scheduleOn(this);
+
+    }
+
+    private void simulateDefectiveLight(){
+        randInt = rand.nextInt(20);
+        if (randInt == 10){
+            this.toggle();
+        }
+    }
+
+
 }
--- src/main/java/sk/tuke/kpi/oop/game/Test.java
+++ src/main/java/sk/tuke/kpi/oop/game/Test.java
@@ -1,11 +1,16 @@
 package sk.tuke.kpi.oop.game;
 
+import java.util.Random;
+
 public class Test {
 
     public static void main(String[] args) {
-
+        Random random = new Random();
         double d = 2.4234;
         System.out.println((int) Math.ceil(1 + 1*1.5));
+        System.out.println( random.nextInt(20));
+        System.out.println( Math.random());
+        System.out.println( Math.random());
         System.out.println("Hello World!");
     }
 }
